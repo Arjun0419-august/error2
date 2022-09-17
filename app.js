260e11e9ce60510252a97dfeefbb97eb978d208b
Arjun Rai <raiarjun0419@gmail.com>
2022-09-17 09:28:12 +0545

    error2

---
+++ app.js
@@ -0,0 +1,28 @@
+
+// require('./db/connect')
+
+const express = require('express');
+const connectDB = require('./db/connect');
+const app = express();
+const tasks = require('./routes/tasks')
+require('dotenv')
+app.use(express.json())
+
+app.get('/hello', (req,res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on ${port}...`))
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
